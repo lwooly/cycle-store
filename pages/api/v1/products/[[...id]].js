ab77a1432c4316d4882101f0c01c1418131e49a1
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2023-12-08 12:22:15 +0000

    product api added

--- pages/api/v1/products/[[...id]].js
+++ pages/api/v1/products/[[...id]].js
@@ -1,5 +1,34 @@
-const handler = async (req, res) => {
-    res.status(200).end();
-};
+import nc from "next-connect"
+import { getProducts, addProduct, updateProduct, removeProduct } from "@/lib/api-functions/server/products/controllers";
+console.log(`next connect`)
+
+const baseRoute = "/api/v1/products/:id?"
+
+const handler = nc({
+    onError: (err, req, res) => {
+        console.error(err.stack);
+        res.status(500).end("something broke!")
+    },
+    onNoMatch: (req, res) => {
+        res.status(404).end("Page is not found");
+    },
+    attachParams: true,
+})
+
+.get(baseRoute, async (req, res) => {
+    getProducts(req, res);
+})
+
+.post(baseRoute,async (req, res) => {
+    addProduct(res, res);
+})
+
+.put(baseRoute, async(req, res) => {
+    updateProduct(req, res);
+})
+
+.delete(baseRoute, async (req, res) => {
+    removeProduct(req, res);
+})
 
 export default handler;

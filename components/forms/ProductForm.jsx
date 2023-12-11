53a0b35a5cb0e8295e4d43f12258a2ffcc0645d3
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2023-12-11 22:25:08 +0000

    preview img added to update forms

--- components/forms/ProductForm.jsx
+++ components/forms/ProductForm.jsx
@@ -4,6 +4,9 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { TextField, Button } from "@/components/mui";
 import { addProductSchema, updateProductSchema } from "@/lib/validation";
 import { ClassSharp } from "@mui/icons-material";
+import Image from "next/image";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import Paragraph from "../Paragraph";
 
 const defaults = {
   image: "",
@@ -15,6 +18,9 @@ const defaults = {
 
 const ProductForm = ({ submitHandler, product }) => {
   const [formValues, setFormValues] = useState(product);
+  const [imgError, setImgError] = useState(false);
+
+  console.log(imgError)
 
   let schema = addProductSchema;
   if (product) {
@@ -27,11 +33,37 @@ const ProductForm = ({ submitHandler, product }) => {
     reset,
     control,
     formState,
+    watch,
   } = useForm({
     resolver: yupResolver(schema),
     mode: "onChange",
     defaultValues: product || defaults,
   });
+
+  // watch image url field
+  const imageUrl = watch("image");
+
+  //reset error status so code checks url when url is changed by user;
+  useEffect(() => {
+    setImgError(false);
+  }, [imageUrl])
+
+  //show image
+  const renderImage = () => {
+    if (imgError) {
+      return (<Paragraph>No image at this url</Paragraph>)
+    } else {
+      return (
+        <Image
+          alt={"Preview image"}
+          src={imageUrl}
+          height={200}
+          width={200}
+          onError={() => setImgError(true)}
+        />
+      );
+    }
+  };
 
   const submitFn = (vals) => {
     setFormValues(vals);
@@ -43,106 +75,109 @@ const ProductForm = ({ submitHandler, product }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit(submitFn)}>
-      <div style={formRowStyle}>
-        <Controller
-          control={control}
-          name="image"
-          render={({ field }) => (
-            <TextField
-              type="text"
-              {...field}
-              label="Image url"
-              fullWidth
-              error={!!errors.image}
-              helperText={errors.image?.message}
-            />
-          )}
-        />
-      </div>
-      <div style={formRowStyle}>
-        <Controller
-          control={control}
-          name="title"
-          render={({ field }) => (
-            <TextField
-              type="text"
-              {...field}
-              label="title"
-              fullWidth
-              error={!!errors.title}
-              helperText={errors.title?.message}
-            />
-          )}
-        />
-      </div>
-      <div style={formRowStyle}>
-        <Controller
-          control={control}
-          name="description"
-          render={({ field }) => (
-            <TextField
-              type="text"
-              {...field}
-              label="description"
-              fullWidth
-              error={!!errors.description}
-              helperText={errors.description?.message}
-            />
-          )}
-        />
-      </div>
-      <div style={formRowStyle}>
-        <Controller
-          control={control}
-          name="price"
-          render={({ field }) => (
-            <TextField
-              type="number"
-              {...field}
-              label="price"
-              fullWidth
-              error={!!errors.price}
-              helperText={errors.price?.message}
-            />
-          )}
-        />
-      </div>
-      <div style={formRowStyle}>
-        <Controller
-          control={control}
-          name="quantity"
-          render={({ field }) => (
-            <TextField
-              type="number"
-              {...field}
-              label="quantity"
-              fullWidth
-              error={!!errors.quantity}
-              helperText={errors.quantity?.message}
-            />
-          )}
-        />
-      </div>
-      <Button
-        type="reset"
-        onClick={() => reset()}
-        primary="true"
-        variant="contained"
-        disabled={!isDirty}
-        sx={{ mx: 2 }}
-      >
-        Reset
-      </Button>
-      <Button
-        type="submit"
-        primary="true"
-        variant="contained"
-        disabled={isSubmitting || !isDirty || (!isDirty && !isValid)}
-      >
-        Submit
-      </Button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(submitFn)}>
+        <div style={formRowStyle}>
+          <Controller
+            control={control}
+            name="image"
+            render={({ field }) => (
+              <TextField
+                type="text"
+                {...field}
+                label="Image url"
+                fullWidth
+                error={!!errors.image}
+                helperText={errors.image?.message}
+              />
+            )}
+          />
+        </div>
+        <div style={formRowStyle}>
+          <Controller
+            control={control}
+            name="title"
+            render={({ field }) => (
+              <TextField
+                type="text"
+                {...field}
+                label="title"
+                fullWidth
+                error={!!errors.title}
+                helperText={errors.title?.message}
+              />
+            )}
+          />
+        </div>
+        <div style={formRowStyle}>
+          <Controller
+            control={control}
+            name="description"
+            render={({ field }) => (
+              <TextField
+                type="text"
+                {...field}
+                label="description"
+                fullWidth
+                error={!!errors.description}
+                helperText={errors.description?.message}
+              />
+            )}
+          />
+        </div>
+        <div style={formRowStyle}>
+          <Controller
+            control={control}
+            name="price"
+            render={({ field }) => (
+              <TextField
+                type="number"
+                {...field}
+                label="price"
+                fullWidth
+                error={!!errors.price}
+                helperText={errors.price?.message}
+              />
+            )}
+          />
+        </div>
+        <div style={formRowStyle}>
+          <Controller
+            control={control}
+            name="quantity"
+            render={({ field }) => (
+              <TextField
+                type="number"
+                {...field}
+                label="quantity"
+                fullWidth
+                error={!!errors.quantity}
+                helperText={errors.quantity?.message}
+              />
+            )}
+          />
+        </div>
+        <Button
+          type="reset"
+          onClick={() => reset()}
+          primary="true"
+          variant="contained"
+          disabled={!isDirty}
+          sx={{ mx: 2 }}
+        >
+          Reset
+        </Button>
+        <Button
+          type="submit"
+          primary="true"
+          variant="contained"
+          disabled={isSubmitting || !isDirty || (!isDirty && !isValid)}
+        >
+          Submit
+        </Button>
+      </form>
+      <ErrorBoundary>{renderImage()}</ErrorBoundary>
+    </>
   );
 };
 

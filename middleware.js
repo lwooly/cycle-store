318271283569dd3d3840e84fc8ab1d8c7fe5d6ac
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2023-12-15 17:52:37 +0000

    page rbac protection

---
+++ middleware.js
@@ -0,0 +1,33 @@
+// middleware.js
+import {
+  withMiddlewareAuthRequired,
+  getSession,
+} from '@auth0/nextjs-auth0/edge';
+import { NextResponse } from 'next/server';
+import { checkRole } from './lib/utils';
+
+import permissions from '@/lib/api-functions/server/permissions';
+
+const { identifier, roles } = permissions;
+
+export const config = {
+  matcher: '/admin/(.*)',
+};
+
+export default withMiddlewareAuthRequired(async function middleware(req) {
+  try {
+    const res = NextResponse.next();
+    const session = await getSession(req, res);
+    const isAdmin = checkRole(session.user, identifier, roles);
+
+    if (!isAdmin) {
+      console.log(`Not admin`);
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+
+    return res;
+  } catch (err) {
+    console.log(err);
+    NextResponse.redirect(new URL('/api/auth/login', req.url));
+  }
+});
